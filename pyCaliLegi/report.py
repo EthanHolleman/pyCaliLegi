c5ef763a74a5093a7a8d8b917e15875f5f84138d
Ethan Holleman <eholleman@luc.edu>
2021-02-23 21:20:05 -0800

    add write csv function

--- pyCaliLegi/report.py
+++ pyCaliLegi/report.py
@@ -1,5 +1,7 @@
 from pyCaliLegi.bill import *
 from pyCaliLegi.summarize import summarize_bill
+import pandas as pd
+
 
 def bill_url_to_LAC_row(url, browser):
     browser.get(url)
@@ -13,11 +15,16 @@ def bill_url_to_LAC_row(url, browser):
     browser.get(history_page_link)
     bill_dict['Brief Status'] = format_last_action_tuple(
         get_last_action_from_history_page(browser)
-        )
-    
+    )
+
     # parse the status page
     browser.get(status_page_link)
     bill_dict.update(parse_status_page(browser))
     bill_dict.update({'url': url})
 
     return bill_dict
+
+
+def write_csv_from_bill_dicts(bill_dicts, filepath):
+    pd.DataFrame.from_dict(bill_dicts).to_csv(str(filepath))
+    return filepath
