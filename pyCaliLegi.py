2b16c971aa40df57e2fb9adccb50d12713b682c6
Ethan Holleman <eholleman@luc.edu>
2021-02-23 21:19:41 -0800

    Add argument parser

--- pyCaliLegi.py
+++ pyCaliLegi.py
@@ -1,27 +1,22 @@
-from pyCaliLegi import search
-from pyCaliLegi import report
+import pandas as pd
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-import pandas as pd
 
-
+from pyCaliLegi import URL, args, report, search
 
 
 def main():
-    pass
+    cmd_args = args.get_args()
+    browser = search.create_browser(URL, headless=cmd_args.headless)
+    browser.search.search_for_bills(
+        browser, cmd_args.and_keywords, cmd_args.or_keywords)
+    bill_urls = search.collect_all_bill_urls_from_search_results(browser)
+    bill_dicts = [
+        report.bill_url_to_LAC_row(bill_url, browser)
+        for bill_url in bill_urls
+    ]
+    report.write_csv_from_bill_dicts(bill_dicts, cmd_args.outpath)
+
 
 if __name__ == '__main__':
     main()
-
-
-url = 'https://leginfo.legislature.ca.gov/faces/billSearchClient.xhtml'
-browser = search.create_browser(url, headless=False)
-browser = search.search_for_bills(browser, ['housing'])
-bill_urls = search.collect_all_bill_urls_from_search_results(browser)
-
-bill_dicts = []
-for bill_url in bill_urls:
-    bill_dicts.append(report.bill_url_to_LAC_row(bill_url, browser))
-
-pd.DataFrame.from_dict(bill_dicts).to_csv('test.csv')
-    
--- pyCaliLegi/args.py
+++ pyCaliLegi/args.py
@@ -2,4 +2,11 @@ import argparse
 
 
 def get_args():
-    pass
+    parser = argparse.ArgumentParser('A small selenium based Python program built by the UC Davis Legeslative Affairs Committee to help automate California legistlation tracking.')
+    parser.add_argument('outpath', metavar='O', help='Path to output file (csv).')
+    parser.add_argument('-and', '--and_keywords', type=str, nargs='+', help='Keywords to search for bills by using "AND" qualifier. Specify up to three words.')
+    parser.add_argument('-or', '--or_keywords', type=str, nargs='+', help='Keywords to search for bills by using "OR" qualifier. Specify up to three words.')
+    parser.add_argument('-sum_size', '--max_summary_size', type=int, help='Max number of sentences in include in extended summary.')
+    parser.add_argument('-headless', '--headless', action='store_true', help='Run browser in headless mode.')
+
+    return parser.parse_args()
